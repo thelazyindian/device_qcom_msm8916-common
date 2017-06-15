35cec287667a7939e017d8b1414bb4c60a81dec0
thelazyindian <satyabrat.me@gmail.com>
2017-06-15 14:30:54 +0000

    msm8916-common: Nuke perfd

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -8,10 +8,6 @@ endif
 PRODUCT_PACKAGES += \
     power.msm8916
 
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so
-
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8916)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.min_freq_0=800000
