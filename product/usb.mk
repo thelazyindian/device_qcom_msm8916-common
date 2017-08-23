e565b8202667ea92aae377102ab47c48dd3a7186
Luca Stefani <luca020400@lineageos.org>
2017-08-24 01:49:02 +0800

    msm8916-common: Build USB HAL

--- product/usb.mk
+++ product/usb.mk
@@ -11,3 +11,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
